d71905014b364034e56980962f4b2fe25c572053
Hector Garza <hggumich@gmail.com>
2022-05-03 18:31:51 -0400

    reset code

--- src/index.js
+++ src/index.js
@@ -1,39 +1,10 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-function Header(props) {
-  return <h1>Hello {props.username}</h1>;
-}
-
-function Layout(props) {
-  return <div style={{ background: "palegoldenrod" }}>{props.children}</div>;
-}
-
-function Login() {
-  return <p>Please Login!</p>;
-}
-
-function Signout() {
-  return <button>Signout</button>;
-}
-
 function App() {
-  return (
-    <Layout>
-      {isAuthenticated ? (
-        <React.Fragment>
-          <Header username="John" />
-          <Signout />
-        </React.Fragment>
-      ) : (
-        <Login />
-      )}
-      <footer>Copyright 2022</footer>
-    </Layout>
-  );
+  const person = "Jane";
+  return <p>{person}</p>;
 }
 
 const rootNode = document.getElementById("root");
-const isAuthenticated = true;
-
 ReactDOM.render(<App />, rootNode);
