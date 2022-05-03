0564ede1f3be8c39ec94ac4845c54e2eac270d5d
Hector Garza <hggumich@gmail.com>
2022-05-03 17:47:49 -0400

    JSX requires different attributes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-const greeting = (
-  <div>
-    <h1>Hello</h1>
-    <p>Welcome to React</p>
-  </div>
-);
+const greeting = <button className="button">Submit</button>;
 const rootNode = document.getElementById("root");
 
 ReactDOM.render(greeting, rootNode);
