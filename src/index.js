ba4cce2f8774407ef63d8b80252f4ee974aff509
Hector Garza <hggumich@gmail.com>
2022-05-03 17:50:01 -0400

    JSX requires {{}} for styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-const greeting = <button className="button">Submit</button>;
+const greeting = <button style={{ background: "red" }}>Submit</button>;
 const rootNode = document.getElementById("root");
 
 ReactDOM.render(greeting, rootNode);
