272f3a6c34bda0d63f267f190fa59f85b3ff9ddb
Hector Garza <hggumich@gmail.com>
2022-05-03 18:35:27 -0400

    array and mapping

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from "react-dom";
 
 function App() {
-  const person = "Jane";
-  return <p>{person}</p>;
+  const people = ["John", "Dave", "Jane"];
+
+  return people.map((person) => <p>{person}</p>);
 }
 
 const rootNode = document.getElementById("root");
