a45263425d4bab4bb911791fd9de3fc4bce65de7
Hector Garza <hggumich@gmail.com>
2022-05-03 17:44:21 -0400

    Use a nested HTML

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-const year = 2022;
-const greeting = <div>Hello React in year {year}</div>;
+const greeting = (
+  <div>
+    <h1>Hello</h1>
+    <p>Welcome to React</p>
+  </div>
+);
 const rootNode = document.getElementById("root");
 
 ReactDOM.render(greeting, rootNode);
