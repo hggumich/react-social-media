355cebb6c65b9624025350e6a18fd7a704260176
Hector Garza <hggumich@gmail.com>
2022-05-03 18:54:53 -0400

    handle input change

--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@ import ReactDOM from "react-dom";
 function App() {
   const people = ["John", "Dave", "Jane"];
 
+  function handleInputChange(event) {
+    const inputValue = event.target.value;
+    console.log(inputValue);
+  }
+
   return (
     <ul>
       {people.map((person, i) => (
         <Person key={i} person={person} />
       ))}
+      <input onChange={handleInputChange} />
     </ul>
   );
 }
 
 function Person(props) {
-  function handlePersonClick(event) {
+  function handlePersonClick() {
     alert(props.person);
-    console.log(event);
   }
   return <li onClick={handlePersonClick}>{props.person}</li>;
 }
