13a1907031045579d7e1c55cbca9da3350e5a5e8
Hector Garza <hggumich@gmail.com>
2022-05-03 18:09:35 -0400

    props with children

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ function Header(props) {
   return <h1>Hello {props.username}</h1>;
 }
 
+function Layout(props) {
+  return <div style={{ background: "palegoldenrod" }}>{props.children}</div>;
+}
+
 const rootNode = document.getElementById("root");
 
 ReactDOM.render(
-  <div>
+  <Layout>
     <Header username="John" />
     <footer>Copyright 2022</footer>
-  </div>,
+  </Layout>,
   rootNode
 );
