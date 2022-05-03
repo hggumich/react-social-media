9f0cc5688a377c1282130e033a3c1418aa160d2b
Hector Garza <hggumich@gmail.com>
2022-05-03 17:57:44 -0400

    function component syntax

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-function Header() {
+const Header = () => {
   return <h1>Hello React</h1>;
-}
+};
 
 const rootNode = document.getElementById("root");
 
