f2431ec757a45b190c00d4a0593dc5ec8fb78a54
Hector Garza <hggumich@gmail.com>
2022-05-04 17:14:09 -0400

    React Hooks Deep Dive Boiler Plate

--- src/index.js
+++ src/index.js
@@ -2,28 +2,16 @@ import React from 'react';
 import ReactDOM from "react-dom";
 
 function App() {
-  const people = ["John", "Dave", "Jane"];
-
   function handleInputChange(event) {
     const inputValue = event.target.value;
     console.log(inputValue);
   }
 
   return (
-    <ul>
-      {people.map((person, i) => (
-        <Person key={i} person={person} />
-      ))}
+    <div>
       <input onChange={handleInputChange} />
-    </ul>
+    </div>
   );
-}
-
-function Person(props) {
-  function handlePersonClick() {
-    alert(props.person);
-  }
-  return <li onClick={handlePersonClick}>{props.person}</li>;
 }
 
 const rootNode = document.getElementById("root");
