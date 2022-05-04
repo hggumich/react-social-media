a84fabe384bd6a62cbd3ad08ddc35b366a188e31
Hector Garza <hggumich@gmail.com>
2022-05-04 17:57:31 -0400

    mulpt hooks using useState

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom";
 
 function App() {
   const [language, setLanguage] = React.useState("Python");
+  const [yearsExperience, setYearsExperience] = React.useState(0);
 
   return (
     <div>
       <button onClick={() => setLanguage("javascript")}>Change Language</button>
+
+      <div>
+        <input
+          type="number"
+          onChange={(event) => setYearsExperience(event.target.value)}
+        />
+      </div>
       <p>I am learning {language}</p>
+      <p>I have {yearsExperience} years</p>
     </div>
   );
 }
