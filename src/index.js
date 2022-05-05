ff411b7b37ba45af12b3ae9277b4f0423c91e25b
Hector Garza <hggumich@gmail.com>
2022-05-05 18:43:39 -0400

    use useEffect to call an API with search

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-const endpoint = "https://api.github.com/users/hggumich";
+const baseUrl = "https://api.github.com/users/";
 
 function App() {
+  const [username, setUsername] = React.useState("hggumich");
   const [user, setUser] = React.useState(null);
-  React.useEffect(async () => {
-    async function getUser() {
-      const response = await fetch(endpoint);
-      const data = await response.json();
-      setUser(data);
-    }
-    getUser();
-    
+
+  async function getUser(route) {
+    const response = await fetch(`${baseUrl}${route}`);
+    const data = await response.json();
+    setUser(data);
+  }
+
+  React.useEffect(() => {
+    getUser(username);
   }, []);
 
-  return user ? (
+  return (
     <div>
-      <h2>{user.name}</h2>
+      <input
+        type="text"
+        placeholder="Input username"
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <button onClick={() => getUser(username)}>Search</button>
+      <button>Clear</button>
+      {user ? (
+        <div>
+          <h2>{user.name}</h2>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-  ) : (
-    <p>Loading...</p>
   );
 }
 
