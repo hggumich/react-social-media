0d731fc90e3b63b57d64b002052badc841018413
Hector Garza <hggumich@gmail.com>
2022-05-05 18:50:32 -0400

    use useRef to clear box

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ const baseUrl = "https://api.github.com/users/";
 function App() {
   const [username, setUsername] = React.useState("hggumich");
   const [user, setUser] = React.useState(null);
+  const searchInput = React.useRef();
 
-  async function getUser(route) {
-    const response = await fetch(`${baseUrl}${route}`);
+  function handleClearInput() {
+    searchInput.current.value = "";
+    searchInput.current.focus();
+  }
+
+  async function getUser() {
+    const response = await fetch(`${baseUrl}${username}`);
     const data = await response.json();
     setUser(data);
   }
 
   React.useEffect(() => {
-    getUser(username);
+    getUser();
   }, []);
 
   return (
@@ -23,9 +29,10 @@ function App() {
         type="text"
         placeholder="Input username"
         onChange={(event) => setUsername(event.target.value)}
+        ref={searchInput}
       />
       <button onClick={() => getUser(username)}>Search</button>
-      <button>Clear</button>
+      <button onClick={handleClearInput}>Clear</button>
       {user ? (
         <div>
           <h2>{user.name}</h2>
