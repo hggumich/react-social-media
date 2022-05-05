2be416ad62a39ef60f0409ebc53f44ff00e8280d
Hector Garza <hggumich@gmail.com>
2022-05-05 18:05:32 -0400

    use useEffect to call an API

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
+const endpoint = "https://api.github.com/users/hggumich";
+
 function App() {
-  const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
-
   React.useEffect(() => {
-    document.addEventListener("mousemove", handleMouseMove);
-
-    return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-    };
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }, []);
 
-  function handleMouseMove(event) {
-    setMousePosition({ x: event.pageX, y: event.pageY });
-  }
-
-  return (
-    <div>
-      <p>
-        X: {mousePosition.x}, Y: {mousePosition.y}{" "}
-      </p>
-    </div>
-  );
+  return <div>user</div>;
 }
 
 const rootNode = document.getElementById("root");
 ReactDOM.render(<App />, rootNode);
-
-function NewPage() {
-  return <div>New page</div>;
-}
-
-setTimeout(() => ReactDOM.render(<NewPage />, rootNode), 2000);
