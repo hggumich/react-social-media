394cc1d81d14df1eeb1c19454675140dd36d737c
Hector Garza <hggumich@gmail.com>
2022-05-06 16:31:11 -0400

    Login Component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./components/Login";
+
+function App() {
+  const [user, setUser] = React.useState("");
+
+  if (!user) {
+    return <Login setUser={setUser} />;
+  }
+  return <div>app</div>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-function App() {}
+import App from "./App";
 
 const rootNode = document.getElementById("root");
 ReactDOM.render(<App />, rootNode);
