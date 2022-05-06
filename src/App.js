4d64e7d272526211e983858e09c69a958109470c
Hector Garza <hggumich@gmail.com>
2022-05-06 16:49:23 -0400

    Login Component, Header, CreatePost

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Login from "./components/Login";
 import Header from "./components/Header";
+import CreatePost from "./components/CreatePost";
 
 function App() {
   const [user, setUser] = React.useState("");
+
+  React.useEffect(() => {
+    document.title = user ? `${user}'s Feed` : "Please login";
+  }, [user]);
 
   if (!user) {
     return <Login setUser={setUser} />;
@@ -11,6 +16,7 @@ function App() {
   return (
     <div>
       <Header user={user} setUser={setUser} />
+      <CreatePost />
     </div>
   );
 }
