28d81e89d1a3d514379913be6ade757b77a6c76e
Hector Garza <hggumich@gmail.com>
2022-05-08 08:50:19 -0400

    use PostList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function PostList() {
-  return <div>PostList</div>;
+function PostList({ posts }) {
+  return posts.map((post, i) => (
+    <React.Fragment key={i}>
+      {post.image && (
+        <img
+          style={{ height: 100, width: 200, objectFit: "cover" }}
+          src={URL.createObjectURL(post.image)}
+          alt="Post Cover"
+        />
+      )}
+      <p>{post.content}</p>
+      <div>{post.user}</div>
+    </React.Fragment>
+  ));
 }
 
 export default PostList;
